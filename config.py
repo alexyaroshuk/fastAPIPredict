3148a5291963ddadf8d24814fa7822e2e7a2ea54
Alex Yaroshuk <alexander_4288@Yahoo.com>
2023-11-07 00:33:12 +0800

    upd cors

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # config.py
 class Config:
     SECRET_KEY = "justsomerandomstringstrictlyfordevelopment"
-    CORS_ORIGINS = ["http://localhost:3000"]
+    CORS_ORIGINS = ["https://yolo-predict-tester.vercel.app/"]
     DEFAULT_MODEL_NAME = 'ds1_2_best.pt'
     DEFAULT_MODEL_PATH = 'ds1_2_best.pt'
