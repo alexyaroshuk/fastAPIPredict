6f716678dd237ab2d15b2aca21700aa8b9844a8e
Alex Yaroshuk <alexander_4288@Yahoo.com>
2023-11-11 14:51:50 +0800

    dry, covnert to rgb so that png works too

--- main.py
+++ main.py
@@ -31,18 +31,16 @@ os.makedirs(DISK_MODELS_DIR, exist_ok=True)
 # Ensure the models directory exists
 os.makedirs(MODELS_DIR, exist_ok=True)
 
-app.add_middleware(SessionMiddleware, secret_key="justsomerandomstringstrictlyfordevelopment", max_age=3600, same_site="none", https_only=True)
+app.add_middleware(SessionMiddleware, secret_key=Config.SECRET_KEY, max_age=3600, same_site="none", https_only=True)
 
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://yolo-predict-tester-git-dev-alexyaroshuk.vercel.app"],
+    allow_origins=Config.CORS_ORIGINS,
     allow_credentials=True,
     allow_methods=["*"],  # Allows all methods
     allow_headers=["*"],  # Allows all headers
 )
-
-
 
 DEFAULT_MODEL_NAME = Config.DEFAULT_MODEL_NAME
 DEFAULT_MODEL_PATH = Config.DEFAULT_MODEL_PATH
@@ -55,7 +53,6 @@ loaded_model_path = None
 
 async def get_request():
     return Request(scope={}, receive=None)
-
 
 def get_model_info(request: Request = Depends(get_request)):
     model_name = request.session.get('model_name', DEFAULT_MODEL_NAME)
@@ -70,19 +67,15 @@ def get_model_info(request: Request = Depends(get_request)):
 
     return model_name, model_path
 
-
-
 def image_to_base64(image_path: str) -> str:
     with open(image_path, "rb") as f:
         return base64.b64encode(f.read()).decode('utf-8')
-
 
 @app.get("/current_model")
 async def current_model(request: Request):
     model_name, _ = get_model_info(request)
     print("Session data before current_model:", request.session)
     return {"model_used": model_name}
-
 
 @app.get("/download_model")
 async def download_model(request: Request):
@@ -92,83 +85,67 @@ async def download_model(request: Request):
         raise HTTPException(status_code=404, detail="Model file not found")
     return FileResponse(model_path, filename=model_path)
 
+def load_model(model_path: str):
+    if not os.path.exists(model_path):
+        raise HTTPException(status_code=404, detail="Model file not found")
+    try:
+        return YOLO(model_path)
+    except RuntimeError as e:
+        logging.error(f"RuntimeError: {str(e)}")
+        raise HTTPException(
+            status_code=400, detail=f"Error loading model: {str(e)}")
+    except Exception as e:
+        logging.error(f"Exception when loading model: {str(e)}")
+        raise HTTPException(
+            status_code=500, detail=f"Unexpected error: {str(e)}")
+
+def save_model(model, model_path: str):
+    try:
+        torch.save(model, model_path)
+        logging.info(f"Model saved to {model_path} successfully")
+    except Exception as e:
+        logging.error(f"Exception when saving model: {str(e)}")
+        raise HTTPException(
+            status_code=500, detail=f"Unexpected error: {str(e)}")
 
 @app.post("/upload_model")
 async def upload_model(request: Request, model_file: UploadFile = File(...)):
-    
-        # Ensure the disk directory exists
-    os.makedirs(DISK_MODELS_DIR, exist_ok=True)
-    
     global loaded_model
     model_data = io.BytesIO(await model_file.read())
     model = torch.load(model_data)
     model_name = model_file.filename
 
-
-
     # Save the model to a file in the disk directory
-    try:
-        disk_model_path = os.path.join(DISK_MODELS_DIR, model_name)
-        torch.save(model, disk_model_path)
-        logging.info(f"Model {model_name} saved to disk directory successfully")
-    except Exception as e:
-        logging.error(f"Exception when saving to disk directory: {str(e)}")
+    disk_model_path = os.path.join(DISK_MODELS_DIR, model_name)
+    save_model(model, disk_model_path)
 
     # Save the model to a file in the models directory
-    try:
-        models_model_path = os.path.join(MODELS_DIR, model_name)
-        torch.save(model, models_model_path)
-        logging.info(f"Model {model_name} saved to models directory successfully")
+    models_model_path = os.path.join(MODELS_DIR, model_name)
+    save_model(model, models_model_path)
 
-        # Copy the model file from the models directory to the disk directory
-        shutil.copy(models_model_path, disk_model_path)
+    # Copy the model file from the models directory to the disk directory
+    shutil.copy(models_model_path, disk_model_path)
 
-        # Store the model path in the session instead of the model itself
-        request.session['model_path'] = models_model_path
-        request.session['model_name'] = model_name
+    # Store the model path in the session instead of the model itself
+    request.session['model_path'] = models_model_path
+    request.session['model_name'] = model_name
 
-        # Store the model path and name in the global dictionary
-        session_id = request.session.get('id', str(uuid.uuid4()))
-        model_info[session_id] = {'model_path': models_model_path, 'model_name': model_name}
+    # Store the model path and name in the global dictionary
+    session_id = request.session.get('id', str(uuid.uuid4()))
+    model_info[session_id] = {'model_path': models_model_path, 'model_name': model_name}
 
-        # Store the model in the global variable
-        loaded_model = YOLO(models_model_path)  # Load the model using YOLO
-    except RuntimeError as e:  # Correct exception for PyTorch model loading
-        logging.error(f"RuntimeError: {str(e)}")
-        raise HTTPException(
-            status_code=400, detail=f"Error loading model: {str(e)}")
-    except Exception as e:
-        logging.error(f"Exception when saving to models directory: {str(e)}")
-        raise HTTPException(
-            status_code=500, detail=f"Unexpected error: {str(e)}")
-
-    print("Session data after upload_model:", request.session)
-    print("Model info after upload_model:", model_info)
+    # Load the model using YOLO
+    loaded_model = load_model(models_model_path)
 
     return {"message": f"Model {model_name} loaded successfully", "model_name": model_name}
-        
-@app.get("/disk_content")
-async def disk_content():
-    try:
-        content = os.listdir(DISK_DIR)
-        return {"content": content}
-    except Exception as e:
-        raise HTTPException(
-            status_code=500, detail=f"Unexpected error: {str(e)}")
-        
-
 
 @app.post("/select_model")
 async def select_model(request: Request, model_name: str):
     global loaded_model, loaded_model_path
     model_path = os.path.join(DISK_MODELS_DIR, model_name)
 
-    # Check if the model file exists
-    if not os.path.exists(model_path):
-        raise HTTPException(status_code=404, detail="Model file not found")
-
     # Load the model
-    loaded_model = YOLO(model_path)
+    loaded_model = load_model(model_path)
     loaded_model_path = model_path
 
     # Generate a unique session ID
@@ -180,13 +157,18 @@ async def select_model(request: Request, model_name: str):
     # Store the model path and name in the global dictionary
     model_info[session_id] = {'model_path': loaded_model_path, 'model_name': model_name}
 
-    print("Session data after select_model:", request.session)
-    print("Model info after select_model:", model_info)
-
     return {"message": f"Model {model_name} selected successfully", "model_name": model_name, "session_id": session_id}
-
-
-
+        
+@app.get("/disk_content")
+async def disk_content():
+    try:
+        content = os.listdir(DISK_DIR)
+        return {"content": content}
+    except Exception as e:
+        raise HTTPException(
+            status_code=500, detail=f"Unexpected error: {str(e)}")
+        
+    return {"message": f"Model {model_name} selected successfully", "model_name": model_name, "session_id": session_id}
 
 @app.get("/project_structure")
 async def project_structure():
@@ -200,8 +182,6 @@ async def project_structure():
 
     return project_structure
 
-
-
 @app.get("/models")
 async def list_models():
     try:
@@ -210,7 +190,6 @@ async def list_models():
     except Exception as e:
         raise HTTPException(
             status_code=500, detail=f"Unexpected error: {str(e)}")
-
 
 @app.post("/predict")
 async def predict(request: Request, file: UploadFile = File(...)):
@@ -228,7 +207,6 @@ async def predict(request: Request, file: UploadFile = File(...)):
 
     print("Model info before predict:", model_info)
     print("Retrieved model name and path:", model_name, model_path)
-
 
     # If no model has been loaded or if the model has changed, load the model
     if loaded_model is None or loaded_model_path != model_path:
@@ -248,6 +226,9 @@ async def predict(request: Request, file: UploadFile = File(...)):
 
     # Read image file
     image = Image.open(io.BytesIO(await file.read()))
+    
+    # Convert the image to RGB mode
+    image = image.convert("RGB")
 
     # Create a temporary directory
     with tempfile.TemporaryDirectory() as temp_dir:
