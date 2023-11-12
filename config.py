ba67a00090f3c98b6f5e7e2a5fa2ee602c317f81
Alex Yaroshuk <alexander_4288@Yahoo.com>
2023-11-12 22:14:18 +0800

    added images, fixed bugs

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ class Config:
     SECRET_KEY = "justsomerandomstringstrictlyfordevelopment"
     CORS_ORIGINS = ["http://localhost:3000"]
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    DEFAULT_MODEL_NAME = 'yolov8n'
+    DEFAULT_MODEL_NAME = 'best(ds 1.7)'
     DEFAULT_MODEL_PATH = os.path.join(BASE_DIR, 'models', DEFAULT_MODEL_NAME)
