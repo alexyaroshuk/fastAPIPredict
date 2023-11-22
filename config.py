aa7118bbe7a0bba10d940a90648c0f73bb53adfa
Alex Yaroshuk <alexander_4288@Yahoo.com>
2023-11-23 00:52:10 +0800

    update config.py

--- config.py
+++ config.py
@@ -5,6 +5,5 @@ class Config:
     SECRET_KEY = "justsomerandomstringstrictlyfordevelopment"
     CORS_ORIGINS = [
         "https://yolo-predict-tester.vercel.app"]
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     DEFAULT_MODEL_NAME = 'best(ds 1.7)'
-    DEFAULT_MODEL_PATH = os.path.join(BASE_DIR, 'models', DEFAULT_MODEL_NAME)
+    DEFAULT_MODEL_PATH = os.path.join('/disk', 'models', DEFAULT_MODEL_NAME)
