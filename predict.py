00764d71bbaa3b0e286d3eb7445319355fdf8b4a
Alex Yaroshuk <alexander_4288@Yahoo.com>
2023-11-26 20:14:00 +0800

    add video predict to predict.py

--- predict.py
+++ predict.py
@@ -1,3 +1,4 @@
+import gc
 from config import Config
 import os
 from PIL import Image, ImageDraw, ImageFont
@@ -8,12 +9,12 @@ import io
 import base64
 from ultralytics import YOLO
 import pandas as pd
+import cv2
 
 loaded_model = None
 loaded_model_path = None
 
-
-def predict_image(image_path: str):
+def load_model_if_needed():
     global loaded_model, loaded_model_path
 
     DEFAULT_MODEL_NAME = Config.DEFAULT_MODEL_NAME
@@ -31,8 +32,12 @@ def predict_image(image_path: str):
         loaded_model = load_model(model_path)
         loaded_model_path = model_path
 
+    return loaded_model
+
+
+def predict_image(image_path: str):
     # Use the loaded model for prediction
-    model = loaded_model
+    model = load_model_if_needed()
 
     # Read image file
     image = Image.open(image_path)
@@ -67,6 +72,65 @@ def predict_image(image_path: str):
     annotated_image_base64 = image_to_base64_for_image(annotated_image_path)
 
     return df, {'type': 'image', "image": annotated_image_base64, "model_used": model_name}
+
+
+def predict_video(video_path: str):
+    # Use the loaded model for prediction
+    model = load_model_if_needed()
+
+    # Open the video file
+    video = cv2.VideoCapture(video_path)
+
+    # Get the frames per second (fps) of the video
+    fps = video.get(cv2.CAP_PROP_FPS)
+
+    frame_count = 0
+    all_results = []  # List to store results for all frames
+
+    # Create a directory for storing the frames
+    frames_dir = 'frames'
+    os.makedirs(frames_dir, exist_ok=True)
+
+    while video.isOpened():
+        ret, frame = video.read()
+        if not ret:
+            break
+
+        # Process every 'fps' frames (i.e., every 2 seconds)
+         if frame_count % fps == 0:
+
+            # Reduce the resolution of the frame
+            frame = cv2.resize(frame, (640, 480))
+
+            # Save the frame as an image
+            frame_path = os.path.join(
+                frames_dir, f"frame_{frame_count}.jpg")
+            cv2.imwrite(frame_path, frame)
+
+            # Run inference on the frame
+            results = model(frame_path)  # list of Results objects
+
+            # Get the size of the image
+            image_size = frame.shape[1], frame.shape[0]
+
+            # Process the results for this frame
+            processed_results = process_results(results, image_size)
+
+            # Add results to all_results list
+            all_results.extend(processed_results['instances'])
+
+            # Delete the frame to free up memory
+            del frame
+            gc.collect()
+
+        frame_count += 1
+
+    video.release()
+
+    # Convert the processed results to a pandas DataFrame
+    df = pd.DataFrame(all_results)
+
+    return df
 
 
 def calculate_area(segments, image_size):
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
-from predict import predict_image
+from predict import predict_video
 from IPython.display import display
 
-df, info = predict_image('disk/shared_images/02_014.jpg')
+video_path = 'disk\shared_images\85e2f96b-c575-46da-91ce-08433a8a388a (1).mp4'
+df = predict_video(video_path)
 display(df)
