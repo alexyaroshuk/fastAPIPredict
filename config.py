7b8f00babb5f10ede5ecb3b9476c2d27932200d5
Alex Yaroshuk <alexander_4288@Yahoo.com>
2024-12-10 18:14:40 +0800

    build(deps): update config.cpython-311.pyc, main.cpython-311.pyc, config.py and requirements.txt

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ import os
 class Config:
     SECRET_KEY = "justsomerandomstringstrictlyfordevelopment"
     CORS_ORIGINS = [
-        "https://yolo-predict-tester.vercel.app"]
+        "http://localhost:3000"]
     DEFAULT_MODEL_NAME = 'best(ds 1.7)'
     DEFAULT_MODEL_PATH = os.path.join('/disk', 'models', DEFAULT_MODEL_NAME)
